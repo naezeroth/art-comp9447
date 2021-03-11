8e8f05cbe7e4b139203f8c397913fe380fac13a5
jonesy <3stevenjones3@gmail.com>
2021-03-11 12:24:54 +1100

    init creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Error404 from './components/Error404';
+import Overview from './components/Overview'
 // import Button from 'react-bootstrap/Button';
 // import { Col, Container, Row } from 'react-bootstrap';
 
@@ -19,6 +20,7 @@ class App extends Component {
       <Route path="/" component={Home} exact/>
       <Route path="/login" component={Login} exact/>
       <Route path="/signup" component={SignUp} exact/>
+      <Route path="/overview" component={Overview} exact/>
       <Route path="/" component={Error404} />
     </Switch>
     </BrowserRouter>
---
+++ frontend/src/components/Overview.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+
+class Overview extends Component {
+    render(){
+        return(
+            <div className="">
+                Overview PAGE
+                
+            </div>
+        );
+    }
+}
+
+export default Overview;
