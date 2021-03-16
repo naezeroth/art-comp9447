97fafcf98bb6c79f05e3e045e73b6e5975e08841
Apoorva Bhagchandani <ApoorvaBhagchandani@gmail.com>
2021-03-16 17:34:23 +1100

    finished creatflow page 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Login from './Login';
 import Register from './Register';
 import Overview from './Overview';
+import CreateFlow1 from './CreateFlow';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
@@ -12,6 +13,7 @@ const App = () => (
         <Route path="/" exact component={Login} />
         <Route path="/Register" exact component={Register} />
         <Route path="/Overview" exact component={Overview} />
+        <Route path="/CreateFlow1" exact component={CreateFlow1} />
       </Switch>
     </div>
   </BrowserRouter>
---
+++ frontend/src/CreateFlow.js
@@ -0,0 +1,134 @@
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import "./Overview.css";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import ButtonAppBar from "./buttonAppBar";
+
+import { alignPropType } from "react-bootstrap/esm/DropdownMenu";
+import { NativeSelect } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+export default function CreateFlow1() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    resourcename: "",
+  });
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    setState({
+      ...state,
+      [name]: event.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <div>
+        <ButtonAppBar />
+      </div>
+      <div style={styles.dropDown}>
+        <ul class="navBar">
+          <li>
+            <a href="./">Home</a>
+          </li>
+          <li>
+            <a href="./">Services</a>
+          </li>
+          <li>
+            <a href="./Overview">Overview</a>
+          </li>
+          <li>
+            <a href="./">History</a>
+          </li>
+        </ul>
+      </div>
+      <Container style={styles.container}>
+          <h1>Select resource</h1>
+          <div style={{ alignItems: "center", marginTop: "20vh" }}>
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="resourcename-native-helper">
+              Resource
+            </InputLabel>
+            <NativeSelect
+              value={state.resourcename}
+              onChange={handleChange}
+              inputProps={{
+                name: "resourcename",
+              }}
+            >
+              <option aria-label="None" value="" />
+              <option value={10}>EC2</option>
+              <option value={20}>IAM</option>
+              <option value={30}>S3</option>
+            </NativeSelect>
+
+            <FormHelperText>Select the resource name</FormHelperText>
+          </FormControl>
+        </div>
+        <Button style={{marginBottom:'50vh'}}>Continue </Button>
+      </Container>
+    </div>
+  );
+}
+
+const styles = {
+  title: {
+    backgroundColor: "#12293B",
+    position: "static",
+  },
+
+  headerBar: {
+    display: "flex",
+    // justifyContent: "space-between",
+    // flexFlow: "row wrap",
+  },
+
+  leftBlock: {
+    paddingRight: 10,
+    // flexGrow: 1,
+  },
+
+  rightBlock: {
+    // flex : 2,
+  },
+
+  acName: {
+    color: "inherit",
+    float: "right",
+  },
+
+  mainBody: {
+    backgroundColor: "#C4C4C4",
+  },
+
+  menuList: {
+    float: "right",
+  },
+
+  container: {
+    backgroundColor: "#C4C4C4",
+    height: "70vh",
+    width: "200vh",
+    marginTop: "10vh",
+  },
+};
