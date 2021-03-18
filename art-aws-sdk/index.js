0d7d0c6b1f1f325c4753b8fde5e055e42453d2c3
Augustine Lee <augustine.lee@student.unsw.edu.au>
2021-03-18 20:23:44 +1100

    sendMessage done

--- art-aws-sdk/index.js
+++ art-aws-sdk/index.js
@@ -1,5 +1,7 @@
 // ES6+ example
 const { EC2Client, DescribeInstancesCommand, StopInstancesCommand, ApplySecurityGroupsToClientVpnTargetNetworkCommand } = require("@aws-sdk/client-ec2");
+const { WebClient } = require('@slack/web-api');
+require('dotenv').config();
 
 const AWSClientService = () => {
     const ec2Client = new EC2Client({ region: "ap-southeast-2" });
@@ -9,27 +11,40 @@ const AWSClientService = () => {
             const data = await ec2Client.send(new DescribeInstancesCommand({}));
             sails.log("Success", JSON.stringify(data));
         }
-        catch(err){
+        catch (err) {
             sails.log("Error", err);
         }
     }
+    const sendMessage = async (msg) => {
+        const web = new WebClient(process.env.SLACK_TOKEN);
+        try {
+            await web.chat.postMessage({
+                channel: '#bot-log',
+                text: msg,
+            });
+        }
+        catch (err) {
+            sails.log("Error", err);
+        }
+    }
+
     const stopInstance = async (instanceIds) => {
         try {
             sails.log("Stop command");
-            const data = await ec2Client.send(new StopInstancesCommand({InstanceIds: instanceIds}));
+            const data = await ec2Client.send(new StopInstancesCommand({ InstanceIds: instanceIds }));
             sails.log("Success", JSON.stringify(data));
         }
-        catch(err){
+        catch (err) {
             sails.log("Error", err);
         }
     }
     const ApplySecurityGroupsToClientVpnTargetNetworkCommand = async (instanceIds) => {
         try {
             sails.log("Stop command");
-            const data = await ec2Client.send(new ApplySecurityGroupsToClientVpnTargetNetworkCommand({InstanceIds: instanceIds}));
+            const data = await ec2Client.send(new ApplySecurityGroupsToClientVpnTargetNetworkCommand({ InstanceIds: instanceIds }));
             sails.log("Success", JSON.stringify(data));
         }
-        catch(err){
+        catch (err) {
             sails.log("Error", err);
         }
     }
@@ -39,6 +54,7 @@ const AWSClientService = () => {
         "Stop Instances": stopInstance,
         "Apply Security Groups To Client Vpn Target Network": ApplySecurityGroupsToClientVpnTargetNetworkCommand,
         2: (var1, var2) => sails.log('hello', var1, var2),
+        "Send Message to Slack": sendMessage,
     }
 
     return functionToDict;
